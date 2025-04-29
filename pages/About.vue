19061ddaff615f072d468fb084145dbcce663f82
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2025-04-29 15:29:21 -0300

    Implementação do About
    Aqui, implemento a página about dentro do meu site, organizando as imagens e os svgs. Porém o css vou adicionar posteriormente.

--- pages/About.vue
+++ pages/About.vue
@@ -1,6 +1,201 @@
 <template>
     <Header />
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ullam blanditiis dolorem deserunt nisi accusamus! Corrupti cum optio, corporis minima voluptates ipsum perferendis facilis, omnis vel magnam consectetur, consequatur commodi!
-    </p>
+    <main>
+        <article>
+            <section class="flex flex-col items-center justify-center">
+                <div class="flex flex-col items-center justify-center">
+                    <img src="/imgs/ViniciusSantos.png" alt="">
+                    <div>
+                        <p>Português</p>
+                        <p>Inglês</p>
+                    </div>
+                </div>
+                <div class="aboutDiv-2">
+                    <h1>
+                        Vinicius Santos
+                    </h1>
+                    <p id="subtitulo">
+                        Desenvolvedor Front-End e Designer Front-End
+                    </p>
+                    <nav id="navegacaoAbout" aria-label="Minhas redes sociais">
+                        <ul>
+                            <li>
+                                <a href="https://github.com/RosiestSloth" target="_blank" rel="noopener noreferrer">
+                                    <GitHubIcon class="size-6" /> GitHub
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://linkedin.com/in/seu-perfil" target="_blank" rel="noopener noreferrer">
+                                    <LinkedInIcon class="size-6" /> LinkedIn
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/vinicius___santos1?igsh=cHlrMGN0cWthZXAy&utm_source=qr"
+                                    target="_blank" rel="noopener noreferrer">
+                                    <InstagramIcon class="size-6" /> Instagram
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <p id="texto">
+                        Meu nome é Vinicius e sou um desenvolvedor de Vianópolis, apaixonado por simplificar desafios complexos em soluções eficientes e intuitivas. Meu trabalho abrange aplicações web, experiências interativas e a combinação entre tecnologia e design focado no usuário.
+                    </p>
+                </div>
+            </section>
+            <hr>
+            <section class="aboutSecao-2">
+                <h2>
+                    Meus Trabalhos
+                </h2>
+                <h3>
+                    Wordpress
+                </h3>
+                <p>
+                    Adquiri muita experiência no <strong>desenvolvimento</strong> e na <strong>manutenção</strong> de sites em WordPress, atuando em projetos como o site <a href="https://www.correspondentevianopolino.com.br/site/" target="_blank" rel="noopener noreferrer">Correspondente Vianopolino</a>.
+                </p>
+                <button type="button"
+                    onclick="window.location.href='/pages/Works.html#wordpress'"
+                >
+                    Mais detalhes
+                </button>
+                <h3>
+                    Figma
+                </h3>
+                <p>
+                    Durante o desenvolvimento de diversos protótipos de sites, como o <strong>Follow-Up</strong>, <strong>Correspondente Vianopolino</strong>, este <strong>portfólio</strong> e o projeto Feedback IA, aperfeiçoei minhas habilidades em design e experiência do usuário. Cada projeto me permitiu explorar o <strong>Figma</strong> de maneira profunda, criando interfaces intuitivas e visualmente atraentes, sempre com foco na usabilidade.
+                </p>
+                <button type="button"
+                    onclick="window.location.href='/pages/Works.html#figma'"
+                >
+                    Mais detalhes
+                </button>
+                <h3>
+                    Frameworks
+                </h3>
+                <p>
+                    Adquiri muita experiência trabalhando com <strong>frameworks</strong> em projetos como o <strong>Follow-Up</strong>. Gostei muito de trabalhar com frameworks, especialmente com a integração do <strong>Tailwind CSS</strong>, que me proporcionou muita flexibilidade nas estilizações.
+                </p>
+                <button type="button"
+                    onclick="window.location.href='/pages/Works.html#frameworks'"
+                >
+                    Mais detalhes
+                </button>
+            </section>
+            <section class="aboutSecao-3">
+                <h2>
+                    Soft/Hard Skills
+                </h2>
+                <h3>
+                    Hard Skills
+                </h3>
+                <ul id="hardSkills">
+                    <li>
+                        <strong>React.Js: </strong>Utilização do React.Js para desenvolver layouts web responsivos e modernos.
+                    </li>
+                    <li>
+                        <strong>Vue: </strong>Experiencia com Vue para contrução de designs web eficientes e otimizados.
+                    </li>
+                    <li>
+                        <strong>Tailwind CSS: </strong>Uso do Tailwind CSS para transformar designs em sites responsivos e eficientes.
+                    </li>
+                    <li>
+                        <strong>WordPress: </strong>Criação e manutenção de sites de forma eficientes utilizando o auxilio de plugins e temas.
+                    </li>
+                    <li>
+                        <strong>Figma: </strong>Utilização do Figma para criar interfaces de usuário intuitivas e atraentes.
+                    </li>
+                    <li>
+                        <strong>Git: </strong>Utilização do Git para controle de versões e colaboração em projetos de desenvolvimento.
+                    </li>
+                    <li>
+                        <strong>GitHub: </strong>Utilização do GitHub para hospedar e gerenciar projetos de desenvolvimento.
+                    </li>
+                </ul>
+                <h3>
+                    Soft Skills
+                </h3>
+                <ul>
+                    <li>
+                        <strong>Comunicação: </strong> Comunicação eficaz com times de desenvolvimento e equipe de design.
+                    </li>
+                    <li>
+                        <strong>Inteligência Emocional: </strong> Compreensão e gestão das emoções de forma eficiente.
+                    </li>
+                    <li>
+                        <strong>Adaptabilidade: </strong>Aprendizado e adaptação de novas Hard Skills e Soft Skills.
+                    </li>
+                    <li>
+                        <strong>Gestão de Tempo: </strong>Melhoria da produtividade e redução de estresse.
+                    </li>
+                    <li>
+                        <strong>Trabalho em Equipe: </strong>Colaboração eficiente em projetos de desenvolvimento.
+                    </li>
+                    <li>
+                        <strong>Empatia: </strong>Entendendo e compreendendo as necessidades e sentimentos das pessoas.
+                    </li>
+                    <li>
+                        <strong>Resuloção de Conflitos: </strong>Solução de conflitos de forma pacifica e eficiente.
+                    </li>
+                </ul>
+            </section>
+            <section class="aboutSecao-4">
+                <h2>
+                    Educação e Certificações
+                </h2>
+                <h3>
+                    Educação
+                </h3>
+                <p>
+                    <strong>Bacharelado em Engenharia de Software </strong><br>
+                    <strong>Instituição:</strong> UniEvangélica de Anápolis <br>
+                    <strong>Status:</strong> 5º Período (Em andamento) <br>
+                    <strong>Áreas de Estudo:</strong> Desenvolvimento de software, lógica computacional, metodologias ágeis e fundamentos de programação.
+                </p>
+                <h3>
+                    Certificações
+                </h3>
+                <p>
+                    <strong>Python 1, 2 e 3</strong> <a id="Butn_Certificados" href="../src/arquivos/PDFs/Python.zip" download="Phyton.zip"> <FilesIcon /> Baixar Certificado</a>
+                </p>
+                <p>
+                    <strong>Instituição: </strong>Curso em Vídeo - Gustavo Guanabara <br>
+                    <strong>Status: </strong>Concluído <br>
+                    <strong>Descrição: </strong>Formação completa em <strong>Python</strong>, abordando desde os fundamentos da linguagem até conceitos avançados, como manipulação de dados, bibliotecas e desenvolvimento de scripts automatizados.
+                </p>
+                <p>
+                    <strong>JavaScript </strong> <a id="Butn_Certificados" href="../src/arquivos/PDFs/JavaScript.pdf" download="JavaScript.pdf"> <FilesIcon /> Baixar Certificado</a>
+                </p>
+                <p>
+                    <strong>Instituição: </strong>Curso em Vídeo - Gustavo Guanabara <br>
+                    <strong>Status: </strong>Concluído <br>
+                    <strong>Descrição: </strong>Curso voltado para desenvolvimento web interativo, cobrindo manipulação do <strong>DOM</strong>, enventos e funcionalidades modernas da linguagem.
+                </p>
+                <p>
+                    <strong>HTML5 e CSS3 - Mundo 1, 2, 3, 4 e 5<a id="Butn_Certificados" href="/src/arquivos/PDFs/HTML_e_CSS.zip" download="HTML_e_CSS.zip"> <FilesIcon /> Baixar Certificado</a></strong>
+                </p>
+                <p>
+                    <strong>Instituição: </strong>Curso em Vídeo - Gustavo Guanabara <br>
+                    <strong>Status: </strong> Em andamento (3º Mundo) <br>
+                    <strong>Descrição: </strong>Introdução prática ao desenvolvimento web com foco na criação de páginas estruturadas e estilizadas, utilizando as melhores práticas em <strong>HTML5</strong> e <strong>CSS3</strong>.
+                </p>
+
+                <p>
+                    <strong>Wordpress<a id="Butn_Certificados" href="/src/arquivos/PDFs/HTML_e_CSS.zip" download="HTML_e_CSS.zip"> <FilesIcon /> Baixar Certificado</a></strong>
+                </p>
+                <p>
+                    <strong>Instituição: </strong>Curso em Vídeo - Gustavo Guanabara <br>
+                    <strong>Status: </strong> Em andamento (1º Mundo) <br>
+                    <strong>Descrição: </strong>Aprender a desenvolver sites completos e responsivos utilizando o <strong>WordPress</strong>desde a instalação até a personalização com o editor <strong>Gutenberg</strong>, temas e plugins.
+                </p>
+            </section>
+        </article>
+    </main>
+    <Footer />
 </template>
+
+<script setup lang="js">
+    import FilesIcon from '../public/svgs/Files.svg';
+    import GitHubIcon from '../public/svgs/GitHub.svg';
+    import LinkedInIcon from '../public/svgs/Linkedin.svg';
+    import InstagramIcon from '../public/svgs/Instagram.svg';
+</script>
